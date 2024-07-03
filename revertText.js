869ae191049a8a537c3eb80772e021c792326cbb
Cesar-Ochoa <cesar.ochoacalvillo@gmail.com>
2024-07-03 11:45:32 -0600

    Optimizado

--- revertText.js
+++ revertText.js
@@ -2,12 +2,23 @@ const a = 'text'.split('');
 const b = 'listas'.split('');
 
 class Solucion {
-    revertirTexto(lista) {
+    /* revertirTexto(lista) {
         let revertida = [];
         for (let i = lista.length - 1; i >= 0; i--) {
             revertida.push(lista[i]);
         }
         return revertida;
+    } */
+
+    revertirTexto(lista) {
+        for (let i = 0; i < lista.length / 2; i++) {
+            let ultimoIndice = lista.length - 1 - i;
+            let letra = lista[ultimoIndice];
+
+            lista[ultimoIndice] = lista[i];
+            lista[i] = letra;
+        }
+        return lista;
     }
 }
 
