ab31ad178ab1c3185f59a3c610299cf81eeb4cdc
Cesar-Ochoa <cesar.ochoacalvillo@gmail.com>
2024-07-12 10:55:04 -0600

    comentarios

--- sumaDeDos.js
+++ sumaDeDos.js
@@ -2,35 +2,25 @@ const a = [1,3,8,11,6,21];
 const b = 19;
 
 class Solucion {
-    /* sumaDeDos(lista, x) {
+    sumaDeDos(lista, x) {
         for (let i = 0; i < lista.length; i++) {
-            let v = lista[i];
-            for (let j = 0; j < lista.length; j++ ) {
-                let w = lista[j];
-                if (j <= i){
+            let v = lista[i]; //en la primera iteracion v == 1, despues a 3, luego a 8 , etc.
+            for (let j = 0; j < lista.length; j++ ) { // tambien inicia desde 0, en la segunda iteracion, j sigue valiendo 0
+                let w = lista[j]; //igual que en v
+                if (j <= i){ //aqui evalua si en la primera iteracion j es menor o igual que i, si es menor o igual, 
+                    console.log('i:',i , ' v: ', v);
+                    console.log('j', j, ' w: ', w );
                     continue;
                 }
                 if (v + w === x) {
+                    console.log('i:',i , ' j: ', j);
+                    console.log('v: ',v, ' w: ',w);
                     return [i,j];
                 }
             }
         }
         return [];
-    } */
-
-    sumaDeDos(lista, x) {
-        let mapa = {};
-        for (let i = 0; i < lista.length; i++) {
-            let v = lista[i];
-            let diferencia = x - v;
-            if (mapa.hasOwnProperty(diferencia)) {
-                return [i, mapa[diferencia]];
-            } else {
-                mapa[v] = i;
-            }
-        }
-        return [];
-    }
+    } 
     
 }
 
